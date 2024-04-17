9560803d9c2ef3dc28b4a3be3c4d1744c165bfca
LucasDL11 <lucasdilandro@gmail.com>
2024-04-17 23:35:03 +0200

    Added edit and delete operations

--- src/main/java/ldl/inventories/controller/ProductController.java
+++ src/main/java/ldl/inventories/controller/ProductController.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Controller class for managing product-related HTTP requests.
@@ -17,6 +19,7 @@ import java.util.List;
 @RestController
 // Base URL for this controller
 @RequestMapping("inventory-app")
+// Allowing cross-origin requests from http://localhost:4200
 @CrossOrigin(value = "http://localhost:4200")
 public class ProductController {
     private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
@@ -64,9 +67,53 @@ public class ProductController {
     public ResponseEntity<Product> getProductById(@PathVariable int id) {
         Product product = this.productService.findProductById(id);
         if (product != null) {
-            throw new ResourceNotFoundException("Product not found");
-        } else {
             return ResponseEntity.ok(product);
+        } else {
+            throw new ResourceNotFoundException("Product not found");
         }
+    }
+
+    /**
+     * Updates a product by its ID.
+     *
+     * @param id             The ID of the product to update
+     * @param productReceived The updated product information
+     * @return ResponseEntity containing the updated product if found, or a ResourceNotFoundException if not found
+     */
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Product> updateProduct(
+            @PathVariable int id,
+            @RequestBody Product productReceived) {
+        Product product = this.productService.findProductById(id);
+        if (product == null) {
+            throw new ResourceNotFoundException("ProductID not found: " + id);
+        }
+        // Update the product information
+        product.setDescription(productReceived.getDescription());
+        product.setPrice(productReceived.getPrice());
+        product.setQuantity(productReceived.getQuantity());
+        // Save the updated product
+        this.productService.saveProduct(product);
+        return ResponseEntity.ok(product);
+    }
+
+    /**
+     * Deletes a product by its ID.
+     *
+     * @param id The ID of the product to delete
+     * @return ResponseEntity containing a map indicating the success of the deletion operation
+     */
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteProduct(@PathVariable int id) {
+        Product product = productService.findProductById(id);
+        if (product == null) {
+            throw new ResourceNotFoundException("ProductID not found: " + id);
+        }
+        // Delete the product
+        this.productService.deleteProductById((product.getProductId()));
+        // Prepare response indicating successful deletion
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
     }
 }
